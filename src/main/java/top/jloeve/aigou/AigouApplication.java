c098631e8b84d7a73f56cbfd1d98af26c2e23538
jloeve <jianglufull@foxmail.com>
2020-07-31 09:36:01 +0800

    initial commit

---
+++ src/main/java/top/jloeve/aigou/AigouApplication.java
@@ -0,0 +1,13 @@
+package top.jloeve.aigou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AigouApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AigouApplication.class, args);
+  }
+
+}
---
+++ src/test/java/top/jloeve/aigou/AigouApplicationTests.java
@@ -0,0 +1,13 @@
+package top.jloeve.aigou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AigouApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
